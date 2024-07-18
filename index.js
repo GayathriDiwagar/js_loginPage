bbe541ca65b1d91f627b4e2a2d035daec5df9ca8
Gayathri Selvaraj <144448324+GayathriDiwagar@users.noreply.github.com>
2024-07-18 13:02:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,46 +37,56 @@ function forgotTab()
 function register()
 {
     event.preventDefault();
-    var email=document.getElementById("re").value
-    var password=document.getElementById("rp").value
-    var passowrRetype=document.getElementById("rrp").value
-    if(email==""){
-        alert("Email Required");
-        return;
-    }
-    else if (password!==passwordReType){
-        alert("Password don't match. Retype your Password");
-        return;
-    }
-    else if (email.indexOf(email)==-1){
-        emailArray
+    var email = document.getElementById("re").value;
+    var password = document.getElementById("rp").value;
+    var passwordRetype = document.getElementById("rrp").value;
+    if (email == "") {
+      alert("Email required");
+      return;
+    } else if (password == "") {
+      alert("Password required");
+      return;
+    } else if (passwordRetype == "") {
+      alert("Re enter Password required");
+      return;
+    } else if (password != passwordRetype) {
+      alert("Password doesn't match \nRe enter your password");
+      return;
+    } else if (emailArray.indexOf(email) == -1) {
+      emailArray.push(email);
+      passwordArray.push(password);
+  
+      alert(email + "  Thanks for the registration. \nyou can login now");
+      document.getElementById("re").value = "";
+      document.getElementById("rp").value = "";
+      document.getElementById("rrp").value = "";
     }
 }
 function login()
 {
     event.preventDefault();
-    var email=document.getElementById("se").value
-    var password=document.getElementById("sp").value
-    var i=emailArray.indexOf(email);
-    if(emailArray.indexOf(email)==-1){
-        if (email==""){
-            alert("Email Required");
-            return;
+    var email = document.getElementById("se").value;
+    var password = document.getElementById("sp").value;
+    var i = emailArray.indexOf(email);
+
+    if (emailArray.indexOf(email) == -1) {
+        if (email == "") {
+        alert("Email required");
+        return;
         }
-        else if(passwordArray[i]!=password){
-            if (password==""){
-                alert("Password required");
-                return;
-            }
-            alert("Password does not match");
-            return;
+        alert("Email does not exist.");
+        return;
+    } else if (passwordArray[i] != password) {
+        if (password == "") {
+        alert("Password required");
+        return;
         }
-    }  
-    else{
-        alert(`Welcome`+email);
-        document.getElementById("se").value="";
-        document.getElementById("sp").value="";
-    } 
+        alert("Password does not match.");
+    } else {
+        alert(email + " Login successful.. Welcome..");
+        document.getElementById("se").value = "";
+        document.getElementById("sp").value = "";
+    }
 }
 function forgot()
 {
